1e47fe979327bd2bfd5a595e9478828d0b7c27cf
linjunhan <3659771584@qq.com>
2025-09-16 21:58:04 +0800

    例子

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,7 +6,7 @@ import "gantt-task-react/dist/index.css";
 
 // Init
 const App = () => {
-  const [view, setView] = React.useState<ViewMode>(ViewMode.Day);
+  const [view, setView] = React.useState<ViewMode>(ViewMode.DayShift);
   const [tasks, setTasks] = React.useState<Task[]>(initTasks());
   const [isChecked, setIsChecked] = React.useState(true);
   let columnWidth = 65;
