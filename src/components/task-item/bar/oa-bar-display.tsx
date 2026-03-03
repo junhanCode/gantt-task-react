1a82705ee6dd977c21661dc43789ed42cedda4c0
linjunhan <3659771584@qq.com>
2026-03-03 22:07:19 +0800

    已撤销的不需要显示延期时间条

--- src/components/task-item/bar/oa-bar-display.tsx
+++ src/components/task-item/bar/oa-bar-display.tsx
@@ -104,8 +104,8 @@ export const OABarDisplay: React.FC<OABarDisplayProps> = ({
     (statusDescription && statusColors[statusDescription as TaskStatus]) ||
     "#E6E6E6";
 
-  // 除「掛起中」外，所有狀態都允許顯示延期段
-  const canShowDelay = statusDescription !== "掛起中";
+  // 「掛起中」和「已撤銷」不顯示延期段
+  const canShowDelay = statusDescription !== "掛起中" && statusDescription !== "已撤銷";
 
   // finishDate 只在「待驗收」/「已完成」状态下才视为有效完成时间
   const COMPLETION_STATUSES: (TaskStatus | undefined)[] = ["待驗收", "已完成"];
